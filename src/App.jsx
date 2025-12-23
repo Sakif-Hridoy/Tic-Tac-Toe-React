4bf8c94b1016c83489e74d3142fa5df73036d184
Sakif-Hridoy <sakifworkspace@gmail.com>
2025-12-23 20:32:41 +0600

    manage tic tac toe input and ouput states and toggle it

--- src/App.jsx
+++ src/App.jsx
@@ -17,9 +17,19 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xisNext, setXisNext] = useState(true);
 
+  const winner = calculateWinner(squares);
+
+  let status;
+
+  if(winner){
+    status = `Winner: ${winner}`
+  } else {
+    status = "Next Player " + (xisNext ? "X" : "O")
+  }
+
   function handleClick(i) {
 
-    if(squares[i]){
+    if(squares[i] || calculateWinner(squares)){
       return;
     }
 
@@ -36,6 +46,7 @@ export default function Board() {
   }
   return (
     <>
+    <div>{status}</div>
       <div className="flex">
         <Square value={squares[0]} onSquareClick={()=> handleClick(0)}></Square>
         <Square value={squares[1]} onSquareClick={()=> handleClick(1)}></Square>
@@ -53,4 +64,25 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+function calculateWinner(squares){
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ];
+
+  for (let i=0; i<lines.length; i++){
+    const [a,b,c] = lines[i];
+    if(squares[a] && squares[a]=== squares[b] && squares[a]=== squares[c])
+    {
+      return squares[a]
+    }
+  }
 }
