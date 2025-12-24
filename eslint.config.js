adf14967ce3ed63a8f215a34709c420fb71eb68b
Sakif-Hridoy <sakifworkspace@gmail.com>
2025-12-24 22:12:36 +0600

    update eslint.config

--- eslint.config.js
+++ eslint.config.js
@@ -28,6 +28,10 @@ export default [
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
+
+      // ✅ props validation warning বন্ধ
+      'react/prop-types': 'off',
+
       'react/jsx-no-target-blank': 'off',
       'react-refresh/only-export-components': [
         'warn',
--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
+/*
+
+    Game
+    -> Board
+        -> Square
+    -> History
+
+*/
+
 import { useState } from "react";
 
 function Square({ value, onSquareClick }) {
@@ -12,121 +21,144 @@ function Square({ value, onSquareClick }) {
     </button>
   );
 }
-function Board({xisNext, squares, onPlay}) {
-
+function Board({ xisNext, squares, onPlay }) {
   const winner = calculateWinner(squares);
 
   let status;
 
-  if(winner){
-    status = `Winner: ${winner}`
+  if (winner) {
+    status = `Winner: ${winner}`;
   } else {
-    status = "Next Player " + (xisNext ? "X" : "O")
+    status = "Next Player " + (xisNext ? "X" : "O");
   }
 
   function handleClick(i) {
-
-    if(squares[i] || calculateWinner(squares)){
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
 
     const nextSquares = squares.slice();
-    
-    if(xisNext){
-      nextSquares[i] = "X"
+
+    if (xisNext) {
+      nextSquares[i] = "X";
     } else {
-      nextSquares[i] = "O"
+      nextSquares[i] = "O";
     }
     onPlay(nextSquares);
     // console.log("clicked");
   }
   return (
     <>
-    <div>{status}</div>
+      <div>{status}</div>
       <div className="flex">
-        <Square value={squares[0]} onSquareClick={()=> handleClick(0)}></Square>
-        <Square value={squares[1]} onSquareClick={()=> handleClick(1)}></Square>
-        <Square value={squares[2]} onSquareClick={()=> handleClick(2)}></Square>
+        <Square
+          value={squares[0]}
+          onSquareClick={() => handleClick(0)}
+        ></Square>
+        <Square
+          value={squares[1]}
+          onSquareClick={() => handleClick(1)}
+        ></Square>
+        <Square
+          value={squares[2]}
+          onSquareClick={() => handleClick(2)}
+        ></Square>
       </div>
       <div className="flex">
-        <Square value={squares[3]} onSquareClick={()=> handleClick(3)}></Square>
-        <Square value={squares[4]} onSquareClick={()=> handleClick(4)}></Square>
-        <Square value={squares[5]} onSquareClick={()=> handleClick(5)}></Square>
+        <Square
+          value={squares[3]}
+          onSquareClick={() => handleClick(3)}
+        ></Square>
+        <Square
+          value={squares[4]}
+          onSquareClick={() => handleClick(4)}
+        ></Square>
+        <Square
+          value={squares[5]}
+          onSquareClick={() => handleClick(5)}
+        ></Square>
       </div>
       <div className="flex">
-        <Square value={squares[6]} onSquareClick={()=> handleClick(6)}></Square>
-        <Square value={squares[7]} onSquareClick={()=> handleClick(7)}></Square>
-        <Square value={squares[8]} onSquareClick={()=> handleClick(8)}></Square>
+        <Square
+          value={squares[6]}
+          onSquareClick={() => handleClick(6)}
+        ></Square>
+        <Square
+          value={squares[7]}
+          onSquareClick={() => handleClick(7)}
+        ></Square>
+        <Square
+          value={squares[8]}
+          onSquareClick={() => handleClick(8)}
+        ></Square>
       </div>
     </>
   );
 }
 
-
+// Default function Game and is exported
 export default function Game() {
-  const [history, setHistory] = useState([Array(9).fill(null)]);
   const [xisNext, setXisNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
 
   const currentSquares = history[history.length - 1];
 
   function handlePlay(nextSquares) {
     setXisNext(!xisNext);
-    setHistory([...history, nextSquares]);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
   }
 
-  const moves = history.map((squares,move)=>{
+  function jumpTo(move) {
+    setCurrentMove(move);
+    setXisNext(move % 2 === 0);
+  }
+
+  const moves = history.map((squares, move) => {
     let description;
-    if(move >0){
-      description = `Go to the move # ${move}`
+    if (move > 0) {
+      description = `Go to the move # ${move}`;
     } else {
-      description = `Go to start the game`
+      description = `Go to start the game`;
     }
     return (
       // eslint-disable-next-line react/jsx-key
-      <li key={move}>
-        <button>
-          {
-            description
-          }
-        </button>
+      <li key={move} className="bg-gray-700 text-white">
+        <button onClick={() => jumpTo(move)}>{description}</button>
       </li>
-    )
-  })
+    );
+  });
 
   return (
-  <div>
-     <div>
-      <Board
-        xisNext={xisNext}
-        squares={currentSquares}
-        onPlay={handlePlay}
-      />
+    <div className="flex justify-center p-4">
+      <div className="mr-16">
+        <Board xisNext={xisNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="">
+        <ol className="border border-gray-400 p-1 text-lg">{moves}</ol>
+      </div>
     </div>
-    <div>
-      <ol>{moves}</ol>
-    </div>
-  </div>
   );
 }
 
-
-function calculateWinner(squares){
+function calculateWinner(squares) {
   const lines = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
   ];
 
-  for (let i=0; i<lines.length; i++){
-    const [a,b,c] = lines[i];
-    if(squares[a] && squares[a]=== squares[b] && squares[a]=== squares[c])
-    {
-      return squares[a]
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
     }
   }
 }
